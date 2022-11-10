bbe782a8fb658ee4f3fb2d3022696b634efdb852
mehul <aswarmehul19@siesgst.ac.in>
2022-11-11 03:29:05 +0530

    day01practice

---
+++ Day01/practice01.py
@@ -0,0 +1,24 @@
+print("Hello World")
+
+#String Concatenation
+
+print("Hello"+"Mehul")  #With no space 
+
+print("Hello "+"Mehul") # with Space
+
+print("Hello"+" "+"Mehul") # with a blank string in between
+
+
+#How to take an input
+
+#input("What is your name?")
+
+
+#print("Hello "+input("What is your name?\n"))
+
+
+#variables
+
+name="Mehul"
+
+print(name)
