79371b1b4b0bc0d8fa450090b656ac599f786ecf
mehul <aswarmehul19@siesgst.ac.in>
2022-11-11 03:59:12 +0530

    ProjectDay01

---
+++ Day01/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator.")
+
+city=input("What's name of the city you grew up in?\n")
+
+petName=input("What's your pet's name?\n")
+
+bandName=city+" "+petName
+
+print("Your band name could be"+bandName)
