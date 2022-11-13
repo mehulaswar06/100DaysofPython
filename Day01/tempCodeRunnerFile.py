6e700db9698a783b9ae37955acbf7aec69c15b59
mehul <aswarmehul19@siesgst.ac.in>
2022-11-13 13:46:41 +0530

    BMICalci

--- Day01/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to the Band Name Generator.")
-
-city=input("What's name of the city you grew up in?\n")
-
-petName=input("What's your pet's name?\n")
-
-bandName=city+" "+petName
-
-print("Your band name could be"+bandName)
