10e1437be031ac7ab14fc340846035973ce147dc
mehul <aswarmehul19@siesgst.ac.in>
2022-11-17 19:33:58 +0530

    Day 04 Completed and Updated

--- Day04/Project_04/Stone_Paper_Scissor.py
+++ Day04/Project_04/Stone_Paper_Scissor.py
@@ -29,6 +29,7 @@ scissors = '''
 import random
 
 user_input=int(input("What do you choose? Type 0 for rock, 1 for paper and 2 for scissors "))
+print(user_input)
 
 computer_input=random.randint(0,2)
 
@@ -40,6 +41,7 @@ else:
   print(scissors)
 
 print("Computer chose")
+print(computer_input)
 
 
 if(computer_input==0):
@@ -50,9 +52,13 @@ else:
   print(scissors)
 
 
+
+
 if(user_input==1 and computer_input==0 or user_input==0 and computer_input==2 or user_input==2 and computer_input==1):
   print("You Won!")
 elif(user_input==0 and computer_input==1 or user_input==2 and computer_input==0 or user_input==1 and computer_input==2):
+  print("You lose!")
+elif computer_input>user_input:
   print("You lose!")
 else:
   print("It's a draw!")
